6f2f23e11813606ee3e16b527100be1fcd836ef9
im-adithya <imadithyavardhan@gmail.com>
2023-06-26 16:21:10 +0530

    chore: log and save other cases

--- service.go
+++ service.go
@@ -72,10 +72,10 @@ func (svc *Service) StartSubscription(ctx context.Context, sub *nostr.Subscripti
 						svc.Logger.Error(result.Error)
 						return
 					}
+					nostrEvent.ReplyId = resp.ID
 					if status == nostr.PublishStatusSucceeded {
 						nostrEvent.State = "replied"
 						nostrEvent.RepliedAt = time.Now()
-						nostrEvent.ReplyId = resp.ID
 						svc.db.Save(&nostrEvent)
 						svc.Logger.WithFields(logrus.Fields{
 							"nostrEventId": nostrEvent.ID,
@@ -84,6 +84,26 @@ func (svc *Service) StartSubscription(ctx context.Context, sub *nostr.Subscripti
 							"replyEventId": resp.ID,
 							"appId":        nostrEvent.AppId,
 						}).Info("Published reply")
+					} else if status == nostr.PublishStatusFailed {
+						nostrEvent.State = "failed"
+						svc.db.Save(&nostrEvent)
+						svc.Logger.WithFields(logrus.Fields{
+							"nostrEventId": nostrEvent.ID,
+							"eventId":      event.ID,
+							"status":       status,
+							"replyEventId": resp.ID,
+							"appId":        nostrEvent.AppId,
+						}).Info("Failed to publish reply")
+					} else {
+						nostrEvent.State = "sent"
+						svc.db.Save(&nostrEvent)
+						svc.Logger.WithFields(logrus.Fields{
+							"nostrEventId": nostrEvent.ID,
+							"eventId":      event.ID,
+							"status":       status,
+							"replyEventId": resp.ID,
+							"appId":        nostrEvent.AppId,
+						}).Info("Reply sent but no response from relay (timeout)")
 					}
 				}
 			}()
