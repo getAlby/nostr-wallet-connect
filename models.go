0d4d02355c3dec53c7ee39dd970e9dcd10605139
im-adithya <imadithyavardhan@gmail.com>
2023-10-11 20:29:13 +0530

    feat: add new app creation ui

--- models.go
+++ models.go
@@ -35,10 +35,10 @@ const (
 )
 
 var nip47MethodDescriptions = map[string]string{
-	NIP_47_GET_BALANCE_METHOD: "Read your balance.",
+	NIP_47_GET_BALANCE_METHOD: "Read your balance and transaction history.",
 	NIP_47_PAY_INVOICE_METHOD: "Send payments from your wallet.",
 	NIP_47_MAKE_INVOICE_METHOD: "Create invoices on your behalf.",
-	NIP_47_LOOKUP_INVOICE_METHOD: "Lookup status of created invoices.",
+	NIP_47_LOOKUP_INVOICE_METHOD: "Lookup statuses of created invoices.",
 }
 
 type AlbyMe struct {
