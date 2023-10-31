8acf82e7bb8d78aff79b5dab16f03fec12666478
René Aaron <rene@twentyuno.net>
2023-10-31 18:23:24 +0100

    fix: styles, darkmode, descriptions

--- models.go
+++ models.go
@@ -35,16 +35,16 @@ const (
 )
 
 var nip47MethodDescriptions = map[string]string{
-	NIP_47_GET_BALANCE_METHOD: "Read access to your wallet",
-	NIP_47_PAY_INVOICE_METHOD: "Send payments",
-	NIP_47_MAKE_INVOICE_METHOD: "Create invoices",
+	NIP_47_GET_BALANCE_METHOD:    "Read your balance",
+	NIP_47_PAY_INVOICE_METHOD:    "Send payments",
+	NIP_47_MAKE_INVOICE_METHOD:   "Create invoices",
 	NIP_47_LOOKUP_INVOICE_METHOD: "Lookup statuses of invoices",
 }
 
 var nip47MethodIcons = map[string]string{
-	NIP_47_GET_BALANCE_METHOD: "wallet",
-	NIP_47_PAY_INVOICE_METHOD: "lightning",
-	NIP_47_MAKE_INVOICE_METHOD: "invoice",
+	NIP_47_GET_BALANCE_METHOD:    "wallet",
+	NIP_47_PAY_INVOICE_METHOD:    "lightning",
+	NIP_47_MAKE_INVOICE_METHOD:   "invoice",
 	NIP_47_LOOKUP_INVOICE_METHOD: "search",
 }
 
@@ -204,6 +204,6 @@ type Nip47LookupInvoiceParams struct {
 }
 
 type Nip47LookupInvoiceResponse struct {
-	Invoice     string `json:"invoice"`
-	Paid        bool   `json:"paid"`
+	Invoice string `json:"invoice"`
+	Paid    bool   `json:"paid"`
 }
